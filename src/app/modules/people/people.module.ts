1660d3aff4eb73044b0eece5cacaeed0984ad754
diego04018 <diegoaguilar00@hotmail.es>
2023-08-19 16:00:44 -0600

    added cards

--- src/app/modules/people/people.module.ts
+++ src/app/modules/people/people.module.ts
@@ -8,6 +8,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [RegisteredPersonsComponent, RegisterPersonComponent],
   imports: [
@@ -19,6 +20,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatInputModule,
     MatButtonModule,
     MatProgressSpinnerModule,
+    MatCardModule,
   ],
   exports: [RegisteredPersonsComponent, RegisterPersonComponent],
 })
